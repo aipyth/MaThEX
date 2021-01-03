4cf5c496fd35ecbb9dcaf4c490643b621677faab
Ivan <aipyth.func@gmail.com>
2021-01-03 18:37:20 +0200

    unsuccessful work

--- main.py
+++ main.py
@@ -77,6 +77,18 @@ class Grammar:
                 return rule[1] in ruled
             return rule[1] == ruled
 
+
+    def prod_rules_to_list(self):
+        p = []
+        for d in self.P:
+            if type(self.P[d]) == list:
+                for i in self.P[d]:
+                    p.append((d, i))
+            else:
+                p.append((d, i))
+        return p
+
+
     def CYK(self, w):
         """
         Cock-Younger-Kasami
@@ -107,14 +119,60 @@ class Grammar:
         "Yakovliev Cock"
         N = len(self.D)
         n = len(w)
-        Q = np.empty((N, n, n))
-        # Pkeys = self.P.keys()
+        Q = np.full((N, n, n), False, dtype=bool)
 
-        for d in N:
-            for i in range(N):
+        for d in range(N):
+            for i in range(n):
                 Q[d, i, i] = 1 if self.check_prod_rule((self.D[d], w[i])) else 0
 
+        # print(f"0 - { n = }")
+        # for m in range(1, n):
+        #     print(f"1 - { m = }")
+        #     for i in range(n-m+1):
+        #         j = i + m - 1
+        #         print(f"2 - { i = }, { j =}")
+        #
+        #         for d in range(N):
+        #
+        #             derived = False
+        #             for d_der in self.P.get(self.D[d]):
+        #                 # print(f"4 - { d_der = }, { derived = }")
+        #                 for k in range(i, j):
+        #                     print(f"5 - { k = }")
+        #                     print(d_der)
+        #             # Q[d, i, j] =
+
+
+        for m in range(2, n+1):
+
+            for i in range(1, n-m+2):
+                j = i + m - 1
+                nonterm_prod_rules = list(filter(
+                    lambda x: type(x[1]) == tuple,
+                    self.prod_rules_to_list()))
+                for d in nonterm_prod_rules:
+                    di = self.D._nodes.index(d[0])
+                    d1 = self.D._nodes.index(d[1][0])
+                    d2 = self.D._nodes.index(d[1][1])
+                    # print(di, d1, d2)
+                    for k in range(i, j):
+                        # Q[d, i-1, j-1] = Q[d1, i-1, k-1] and Q[d2, k, j-1]
+                        print(f"{di=}")
+                        print(f"{d1=}, {d2=}, {i=}, {j=}, {k=}")
+                        print(f"{Q[d1, i-1, k-1]=}, {Q[d2, k-1, j-1]=}, {Q[d1, i-1, k-1] and Q[d2, k-1, j-1] = }")
+                        print()
+                        # Q[d, i, j] = Q[d1, i, k] and Q[d2, k, j]
+
+
+                # for d in range(N):
+                #     derived = False
+
+                    # right = self.P.get(self.D[d])
+                    # print(right)
+
+
         print(Q)
+
 
 
 def main():
@@ -129,7 +187,7 @@ def main():
         # d0 -> d1 d1 | d2 d3
         'd0': [('d1', 'd1'), ('d2', 'd3')],
         #d1 -> d1 d1 | d2 d2
-        'd1': [('d1', 'd1'), ('d2', 'd2')],
+        'd1': [('d1', 'd1'), ('d2', 'd3')],
         'd2': '(',
         'd3': [('d1' , 'd4'), ')'],
         'd4': ')'
@@ -138,6 +196,7 @@ def main():
     # print( g.check_prod_rule( ('d0', ('d2', 'd1')) ) )
 
     print(g.CYK('()'))
+    # print(g.prod_rules_to_list())
 
 
 if __name__ == '__main__':
